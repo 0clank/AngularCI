ac5f2605a17c2fecca9fdfb37866a00f56f1b4ef
Danny <25693070+Qu1oX@users.noreply.github.com>
2019-12-25 12:21:10 +0100

    Update protractor-ci.conf.js

--- e2e/protractor-ci.conf.js
+++ e2e/protractor-ci.conf.js
@@ -1,10 +1,10 @@
     const config = require('./protractor.conf').config;
 
     config.capabilities = {
-        browserName: 'chrome',
-        chromeOptions: {
-            args: ['--headless', '--disable-gpu', '--no-sandbox', '--disable-dev-shm-usage']
-        }
+      browserName: 'firefox',
+      firefoxOptions: {
+        args: ['--headless']
+      }
     };
 
     exports.config = config;
