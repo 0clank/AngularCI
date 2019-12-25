9c4b7668b01fb321370a3fda00f9fef7a3d1589a
Danny <25693070+Qu1oX@users.noreply.github.com>
2019-12-25 14:42:27 +0100

    Create protractor-ci.conf.js

---
+++ e2e/protractor-ci.conf.js
@@ -0,0 +1,11 @@
+const config = require('./protractor.conf').config;
+
+config.capabilities = {
+   browserName: 'firefox',
+
+  'moz:firefoxOptions': {
+     args: [ "--headless" ]
+   }
+};
+
+exports.config = config;
